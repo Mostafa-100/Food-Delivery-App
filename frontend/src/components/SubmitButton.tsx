d990bce41591f0b2888ad63b33e57ee2b1b1be2a
Mostafa-100 <imustaphaaourik@gmail.com>
2025-03-08 15:12:24 +0000

    Refactoring SubmitButton.tsx

--- frontend/src/components/SubmitButton.tsx
+++ frontend/src/components/SubmitButton.tsx
@@ -1,4 +1,4 @@
-import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { AiOutlineLoading3Quarters as LoadingIcon } from "react-icons/ai";
 import { FaRegCircleCheck } from "react-icons/fa6";
 
 type ButtonProps = {
@@ -8,31 +8,20 @@ type ButtonProps = {
   successLabel?: string;
 };
 
-function SubmitButton({
-  label,
-  isLoading,
-  isSuccess,
-  successLabel,
-}: ButtonProps) {
+const styleOnLoading = "bg-neutral-500 pointer-events-none";
+const styleOnSuccess = "bg-green-500 pointer-events-none";
+const styleOnFailure = "bg-orange-600 hover:bg-orange-700";
+
+function SubmitButton({ label, isLoading, isSuccess, successLabel }: ButtonProps) {
   return (
     <button
       type="submit"
-      className={`w-full flex items-center justify-center text-white py-2 rounded-lg transition-colors ${
-        isLoading && "bg-neutral-500 pointer-events-none"
-      }
-        ${
-          isSuccess
-            ? "bg-green-500 pointer-events-none"
-            : "bg-orange-600 hover:bg-orange-700"
-        }
-        `}
-    >
+      className={`w-full flex items-center justify-center text-white py-2 rounded-lg transition-colors ${isLoading && styleOnLoading}
+        ${isSuccess ? styleOnSuccess : styleOnFailure}`}>
       <span className="mr-1">
         {isLoading ? "Please wait" : isSuccess ? successLabel : label}
       </span>
-      {isLoading && (
-        <AiOutlineLoading3Quarters className="animate-spin ease-linear text-sm" />
-      )}
+      {isLoading && (<LoadingIcon className="animate-spin ease-linear text-sm" />)}
       {isSuccess && <FaRegCircleCheck className="text-sm" />}
     </button>
   );
